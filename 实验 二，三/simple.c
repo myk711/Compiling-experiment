8b22bc32522eb9cc38f75e5aadefc903aca7bd3a
myk711 <66428706+myk711@users.noreply.github.com>
2021-10-17 21:29:14 +0800

    Add files via upload

---
+++ 实验 二，三/simple.c
@@ -0,0 +1,42 @@
+int a,b,c;
+struct test_struct
+{
+	int x;
+	float y;
+	string name;
+};
+int main()
+{
+	bool x;
+	int alist[10];
+	bool bl = true;
+	int count = 0;
+	for (int i = 0; i < 10; i++)
+	{
+		alist[i] = 10 - i;
+	}
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			if (alist[i] < alist[j])
+			{
+				int temp = alist[i];
+				alist[i] = alist[j];
+				alist[j] = temp;
+			}
+		}
+	}
+	while (bl)
+	{
+		printf("%d",alist[count]);
+		if (count >= 10) break;
+		else continue;
+	}
+}
+struct POINT
+{
+	int x;
+	float y;
+	string name;
+};
